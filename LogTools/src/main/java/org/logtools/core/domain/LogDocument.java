e67709a8d6bb008e0925bbce972d542710a2d45f
chandler song <chandler605@gmail.com>
2012-12-26 22:33:16 +0800

    add log4j dependency and add some domain object

--- LogTools/src/main/java/org/logtools/core/domain/LogDocument.java
+++ LogTools/src/main/java/org/logtools/core/domain/LogDocument.java
@@ -7,4 +7,20 @@ package org.logtools.core.domain;
  */
 public interface LogDocument {
 
+    
+    public void addLine(LogEntry le);
+    
+    
+    /**
+     * if there's one multi-line log. <br>
+     *  One scenario <br>
+     *   one exception log, the first line is 10 and prints 10 lines<br>
+     *   it should return the logEntry, if i want to find log which lineNumber is 15<br>
+     *   
+     * TODO it should be remove form here
+     * 
+     * @param lineNumber
+     * @return
+     */
+    public LogEntry findLog(int lineNumber);
 }
--- LogTools/src/main/java/org/logtools/core/domain/LogEntry.java
+++ LogTools/src/main/java/org/logtools/core/domain/LogEntry.java
@@ -1,5 +1,7 @@
 package org.logtools.core.domain;
 
+import java.util.Date;
+
 /**
  * represent one line log with timestamp, <br>
  * other multi-line log, like exception with track log, should still be one logEntry
@@ -8,4 +10,27 @@ package org.logtools.core.domain;
  */
 public interface LogEntry {
 
+    /**
+     * get the line number of this log<br>
+     * 
+     * if one log entry is multi-line, for example. it's a exception log. it's line number is 10 and  prints 10 lines.<br>
+     * the next LogEntry's lineNumber is 20
+     * @return
+     */
+    public Integer getLineNumber();
+    
+    
+    public Boolean isException();
+    
+    public Boolean isMultiLines();
+    
+    public String getThreadInfo();
+        
+    public Date getTime();
+    
+    public String getMessage();
+    
+    public String getLevel();
+    
+    public String getCatalog();
 }
